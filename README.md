a15f0b76491646898de8227f34bc0f510afbe8c9
Richard Bidin <richardbidin@outlook.com>
2022-06-21 12:39:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Movie DB
+## A react native implementation of the design concept made by [Luis Carlos Zanini](https://www.figma.com/community/file/1059842558004825230).
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	SafeAreaView,
+	Text,
+	View
+} from 'react-native';
+
+function App() {
+	return (
+		<SafeAreaView>
+			<View>
+				<Text>{'The Movie DB'}</Text>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+export default App;
