46a460bbf5cfe3c374a22b6325556ed403e1c978
Richard Bidin <richardbidin@outlook.com>
2023-01-08 11:38:41 -0300

    Adds React Query and minor improvements (#19)
    Co-authored-by: Richard Bidin <richardbidin@Richards-MacBook-Pro.local>

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,4 +1,4 @@
-import { Map, Optional } from 'src/@types';
+import { Optional } from 'src/@types';
 import { MovieFactory } from 'src/models/factories/movie_factory';
 import { MovieByGenre } from 'src/models/movie_by_genre';
 import { MovieDetails } from 'src/models/movie_details';
@@ -6,31 +6,16 @@ import { MovieDetailsDTO, MoviesByGenreDTO } from 'src/models/movie_dtos';
 import { api } from 'src/services/api';
 import { Logger } from 'src/utils/helpers/logger';
 
-const cache: Map<any> = {};
-
 export class Services {
 	private static async apiFetch<T>(endpoint: string, params: any): Promise<Optional<T>> {
-		let data: T | null = null;
-		let cacheKey: string;
-		let cachedData: | undefined;
-
 		try {
-			cacheKey = `${endpoint}/${Object.entries(params).map(param => `${param[0]}=${param[1]}`).sort()}`;
-			cachedData = cache[cacheKey];
-
-			if (cachedData) {
-				return cachedData;
-			}
-
-			data = (await api.get(endpoint, { params })).data;
-			cache[cacheKey] = data;
-
-			Logger.info('fetched', cacheKey, 'at', new Date().toUTCString());
+			const { data } = await api.get(endpoint, { params });
+			return data;
 		} catch (e) {
 			Logger.error(e);
 		}
 
-		return data;
+		return null;
 	}
 
 	public static async getMovieWithDetails(movieId: number | string): Promise<Optional<MovieDetails>> {
